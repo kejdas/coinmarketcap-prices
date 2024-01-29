28c49a20fdcfb665bd11ea9ba0f77a5dc0513de4
kejdas <64081579+kejdas@users.noreply.github.com>
2024-01-29 14:53:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Define the URL of the CoinMarketCap website
 url = "https://coinmarketcap.com/"
+
+# Make a request to the website and retrieve the HTML content
 result = requests.get(url).text
+
+# Parse the HTML content using BeautifulSoup
 doc = BeautifulSoup(result, "html.parser")
 
+# Access the <tbody> element containing the cryptocurrency information
 tbody = doc.tbody
+
+# Extract the list of <tr> (table rows) elements from the <tbody>
 trs = tbody.contents
 
-#prices = {}
-
+# Loop through the first 10 <tr> elements (representing the top 10 cryptocurrencies)
 for tr in trs[:10]:
+    # Extract the name and price from each <tr>
     name, price = tr.contents[2:4]
+
+    # Extract the text content of the <p> tag inside the name element
     fixed_name = name.p.string
+
+    # Extract the text content of the <a> tag inside the price element
     fixed_price = price.a.string
-    # prices[fixed_name] = fixed_price
+
+    # Print the fixed name and price of each cryptocurrency
     print(fixed_name)
     print(fixed_price)
     print()
